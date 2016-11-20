2474ec7d6129ede66927571a780e69bb7c9b878c
ebornier-pivotal <ebornier@pivotal.io>
2016-11-20 18:21:28 +0100

    Update BasicConcourseApplication.java

--- src/main/java/com/example/BasicConcourseApplication.java
+++ src/main/java/com/example/BasicConcourseApplication.java
@@ -17,6 +17,6 @@ public class BasicConcourseApplication {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Orange Gardens!";
+        return "Greetings from Pivotal Labs!";
     }
 }
