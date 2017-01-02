e2584c89adec34487b92261e48a707445e3803e6
erwanbornier <ebornier@pivotal.io>
2017-01-02 15:17:59 +0100

    ii

--- ci/build.sh
+++ ci/build.sh
@@ -2,5 +2,6 @@
 export http_proxy=http://10.12.9.165:8080
 export https_proxy=http://10.12.9.165:8080
 cd basic-concourse-repo
+export
 ./mvnw clean package -Dmaven.test.skip=true 
 mv target/*.jar ../build/
