f81cac943482cccf66f911831da774e0c148d512
erwanbornier <ebornier@pivotal.io>
2017-01-03 14:58:31 +0100

    ii

--- ci/build.sh
+++ ci/build.sh
@@ -1,6 +1,5 @@
 #!/bin/bash 
-export http_proxy=http://10.12.9.165:8080
-export https_proxy=http://10.12.9.165:8080
+export MAVEN_OPTS="-Dhttp.proxyHost=10.12.9.165 -Dhttp.proxyPort=8080 -Dhttps.proxyHost=10.12.9.165 -Dhttps.proxyPort=8080"
 cd basic-concourse-repo
 export
 ./mvnw clean package -Dmaven.test.skip=true 
