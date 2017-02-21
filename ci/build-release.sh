07dbe6ad7cd03d90d280b255fb220bec860ab536
erwanbornier <ebornier@pivotal.io>
2017-02-21 22:56:36 +0100

    add deploy release

--- ci/build-release.sh
+++ ci/build-release.sh
@@ -1,5 +1,4 @@
 #!/bin/bash 
-export MAVEN_OPTS="-Dhttp.proxyHost=10.12.9.165 -Dhttp.proxyPort=8080 -Dhttps.proxyHost=10.12.9.165 -Dhttps.proxyPort=8080"
 cd basic-concourse-release
 tar xvf source.tar.gz --strip 1
 export
--- ci/build.sh
+++ ci/build.sh
@@ -1,5 +1,4 @@
 #!/bin/bash 
-export MAVEN_OPTS="-Dhttp.proxyHost=10.12.9.165 -Dhttp.proxyPort=8080 -Dhttps.proxyHost=10.12.9.165 -Dhttps.proxyPort=8080"
 cd basic-concourse-repo
 export
 ./mvnw clean package -Dmaven.test.skip=true 
