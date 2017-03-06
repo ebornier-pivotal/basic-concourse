8d0708501a1e97cf2a358fc16bb69bf8065b311a
erwanbornier <ebornier@pivotal.io>
2017-03-06 23:01:56 +0100

    add slack alert

--- src/main/java/com/example/BasicConcourseApplication.java
+++ src/main/java/com/example/BasicConcourseApplication.java
@@ -17,6 +17,6 @@ public class BasicConcourseApplication {
 
     @RequestMapping("/")
     public String index() {
-        return "Greetings from FCA!";
+        return "Greetings from Pivotal!";
     }
 }
