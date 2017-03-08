fe0674d88054b91f25110c35471347f7813dc18f
erwanbornier <ebornier@pivotal.io>
2017-03-09 00:10:28 +0100

    add sleuth

--- src/main/java/com/example/BasicConcourseApplication.java
+++ src/main/java/com/example/BasicConcourseApplication.java
@@ -4,11 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 @RestController
 public class BasicConcourseApplication {
+
+  private static Logger log = LoggerFactory.getLogger(BasicConcourseApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BasicConcourseApplication.class, args);
@@ -17,6 +20,7 @@ public class BasicConcourseApplication {
 
     @RequestMapping("/")
     public String index() {
-        return "{\"Greetings from Pivotal!\"}";
+       log.info("Handling greetings"); 
+       return "{\"Greetings from Pivotal!\"}";
     }
 }
